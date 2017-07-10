16d95d33e2727a9da8f82cc1d2f8cd6790ba5268
jermiedomingo <8jermiedomingo@gmail.com>
2017-07-10 10:15:36 +0400

    adding materialize css dependency

--- src/App.js
+++ src/App.js
@@ -1,10 +1,57 @@
 import React, { Component } from 'react';
 import './App.css';
 
+//https://github.com/facebookincubator/create-react-app/issues/679
+//https://stackoverflow.com/questions/41657849/cannot-import-materialize-css-in-react-project-to-use-chips
+import $ from 'jquery/src/jquery';
+import 'materialize-css';
+import 'materialize-css/dist/css/materialize.min.css';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div class="card blue-grey darken-1">
+        <PostItDashboard />
+      </div>
+    );
+  }
+}
+
+class PostItDashboard extends Component {
+  render() {
+
+    return (
+      <div>
+        <NoteItemList />
+        <NoteItemList />
+      </div>
+    );
+  }
+}
+
+class NoteItemList extends Component {
+  render(){
+    return (
+      <div>
+        <NoteItem title="26zv" comment="in sync with prod"/>
+        <NoteItem title="27zv" comment="sync with prod"/>
+      </div>
+    );
+  }
+}
+
+class NoteItem extends Component {
+  render(){
+
+    return (
+      <div>
+        <div>
+          <label>Title</label>
+        <input type='text' defaultValue={this.props.title} />
+        </div>
+        <div>
+          <textarea defaultValue={this.props.comment}></textarea>
+        </div>
       </div>
     );
   }
