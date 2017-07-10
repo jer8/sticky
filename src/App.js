3d0762d8e3f55d88a065949f627d82320b1320a7
jermiedomingo <8jermiedomingo@gmail.com>
2017-07-10 13:42:02 +0400

    more static components

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 
 //https://github.com/facebookincubator/create-react-app/issues/679
 //https://stackoverflow.com/questions/41657849/cannot-import-materialize-css-in-react-project-to-use-chips
-//import $ from 'jquery/src/jquery';
-//import 'materialize-css';
+import $ from 'jquery/src/jquery';
+import 'materialize-css';
 import 'materialize-css/dist/css/materialize.min.css';
 import 'masonry-layout';
 
@@ -72,25 +72,34 @@ class PostItDashboard extends Component {
 
 class NoteItemList extends Component {
 
+  constructor(props){
+    super(props);
+
+    this.state = {
+      isFormOpen: false
+    };
+  }
+
   render() {
-    console.log(this.props.notes);
+
+    const {isFormOpen} = this.state;
+
+    const noteItems = isFormOpen
+      ? (this.props.notes.map((note) => <NoteItemForm about={note.about} comment={note.comment}/>))
+      : (this.props.notes.map((note) => <NoteItem about={note.about} comment={note.comment}/>));
+
+    const cardButtons = isFormOpen? <CardActions /> : <CardConfirmations />;
+
     return (
-      <div className=" col m6 s12">
+      <div className="col m6 s12">
         <div className="card">
           <div className="card-content">
             <span className="card-title">{this.props.title}</span>
             <ul className="collection">
-              {
-                this.props.notes.map((note) =>
-                  <NoteItem about={note.about} comment={note.comment}/>
-                )
-              }
+              { noteItems }
             </ul>
           </div>
-          <div className="card-action right-align">
-            <a href="#">UPDATE</a>
-            <a href="#">REMOVE</a>
-          </div>
+          {cardButtons}
         </div>
       </div>
     );
@@ -98,8 +107,8 @@ class NoteItemList extends Component {
 }
 
 class NoteItem extends Component {
-  render() {
 
+  render() {
     return (
       <li className="collection-item">
         <span className="title">{this.props.about}</span>
@@ -114,19 +123,45 @@ class NoteItem extends Component {
 class NoteItemForm extends Component {
 
   render() {
+
+    const {about, comment} = this.props;
+
     return (
       <li className="collection-item">
         <div className="input-field">
-          <input id='1' type='text' defaultValue={this.props.about}/>
+          <input id='1' type='text' defaultValue={about}/>
           <label for='1'>Title</label>
         </div>
         <div className="input-field">
-          <textarea id="textarea1" defaultValue={this.props.comment} className="materialize-textarea"></textarea>
+          <textarea id="textarea1" defaultValue={comment} className="materialize-textarea"></textarea>
           <label for="textarea1">Notes</label>
         </div>
       </li>
     );
   }
 }
+
+class CardActions extends Component {
+  render() {
+    return (
+      <div className="card-action right-align">
+        <a href="#">UPDATE</a>
+        <a href="#">DELETE</a>
+      </div>
+    );
+  }
+}
+
+class CardConfirmations extends Component {
+  render() {
+    return (
+      <div className="card-action right-align">
+        <a href="#">SAVE</a>
+        <a href="#">CANCEL</a>
+      </div>
+    );
+  }
+}
+
 
 export default PostItDashboard;
