944262ce7c8e36f9d0f8954c08dcd1a8cbf99a76
jermiedomingo <8jermiedomingo@gmail.com>
2017-07-10 13:18:41 +0400

    more static rendering

--- src/App.js
+++ src/App.js
@@ -1,60 +1,124 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
 //https://github.com/facebookincubator/create-react-app/issues/679
 //https://stackoverflow.com/questions/41657849/cannot-import-materialize-css-in-react-project-to-use-chips
-import $ from 'jquery/src/jquery';
-import 'materialize-css';
+//import $ from 'jquery/src/jquery';
+//import 'materialize-css';
 import 'materialize-css/dist/css/materialize.min.css';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="container col">
-        <PostItDashboard />
-      </div>
-    );
-  }
-}
+import 'masonry-layout';
 
 class PostItDashboard extends Component {
+
+  constructor(props) {
+     super(props);
+
+     this.state = {
+       postIts: [{
+         title:'flystaging7',
+          notes: [
+            {
+              about: '26zv',
+              comment: 'in sync with prod'
+            },
+            {
+              about: 'DB',
+              comment: 'in sync with prod'
+            },
+          ],
+       },
+      {
+        title:'flystaging5',
+         notes: [
+           {
+             about: '40zv/41zv',
+             comment: 'with 17.2 builds'
+           },
+           {
+             about: 'DB',
+             comment: 'with 17.2 changes'
+           },
+         ],
+
+      },
+      {
+        title:'flystaging10',
+         notes: [
+           {
+             about: 'iis',
+             comment: 'paid seat'
+           },
+           {
+             about: 'DB',
+             comment: 'with paid seat changes'
+           },
+         ],
+
+      }],
+     };
+   }
+
   render() {
 
     return (
-      <div className="row">
-        <NoteItemList listTitle="List 1"/>
-        <NoteItemList listTitle="List 2"/>
-        <NoteItemList listTitle="List 3"/>
+      <div className="container row">
+        {this.state.postIts.map((postIt) =>
+          <NoteItemList title={postIt.title} notes={postIt.notes}/>
+        )}
       </div>
     );
   }
 }
 
 class NoteItemList extends Component {
-  render(){
+
+  render() {
+    console.log(this.props.notes);
     return (
-      <div className="card col m6 s12">
-        <div className="card-content">
-          <span className="card-title">{this.props.listTitle}</span>
-          <ul className="collection teal">
-            <NoteItem title="item1Title" comment="item1Comment"/>
-            <NoteItem title="item2Title" comment="item2Comment"/>
-          </ul>
+      <div className=" col m6 s12">
+        <div className="card">
+          <div className="card-content">
+            <span className="card-title">{this.props.title}</span>
+            <ul className="collection">
+              {
+                this.props.notes.map((note) =>
+                  <NoteItem about={note.about} comment={note.comment}/>
+                )
+              }
+            </ul>
+          </div>
+          <div className="card-action right-align">
+            <a href="#">UPDATE</a>
+            <a href="#">REMOVE</a>
+          </div>
         </div>
       </div>
-
     );
   }
 }
 
 class NoteItem extends Component {
-  render(){
+  render() {
 
     return (
       <li className="collection-item">
+        <span className="title">{this.props.about}</span>
+        <p>
+          {this.props.comment}
+        </p>
+      </li>
+    );
+  }
+}
+
+class NoteItemForm extends Component {
+
+  render() {
+    return (
+      <li className="collection-item">
         <div className="input-field">
-          <input id='1' type='text' defaultValue={this.props.title}/>
-          <label for='1'>Item Name</label>
+          <input id='1' type='text' defaultValue={this.props.about}/>
+          <label for='1'>Title</label>
         </div>
         <div className="input-field">
           <textarea id="textarea1" defaultValue={this.props.comment} className="materialize-textarea"></textarea>
@@ -65,4 +129,4 @@ class NoteItem extends Component {
   }
 }
 
-export default App;
+export default PostItDashboard;
