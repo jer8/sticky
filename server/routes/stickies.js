8e874c0e39675c9d6a71fcc807d5780b15ddd5b5
jermiedomingo <8jermiedomingo@gmail.com>
2017-08-01 21:31:16 +0400

    adds persistence to data.json local file and api endpoints

--- server/routes/stickies.js
+++ server/routes/stickies.js
@@ -1,15 +1,64 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+const path = require('path');
 
-/* GET notes listing. */
-router.get('/', function(req, res, next) {
-  res.json([
-    {
-      id: '1',
-      title: 'Sticky Sample',
-      notes: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed erat rhoncus dolor laoreet efficitur. Aenean laoreet justo libero, nec rhoncus sapien scelerisque ut. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Maecenas ipsum felis, feugiat rhoncus lacinia ac, dapibus ut mi. Fusce et laoreet neque'
-    },
-  ]);
+const DATA_FILE = path.join(__dirname, '../data.json');
+
+/* GET stickies listing. */
+router.get('/', (req, res, next) => {
+  fs.readFile(DATA_FILE, (err, data) => {
+    res.setHeader('Cache-Control', 'no-cache');
+    res.json(JSON.parse(data));
+  });
+});
+
+/* SAVE a sticky. */
+router.post('/', (req, res, next) => {
+  fs.readFile(DATA_FILE, (err, data) => {
+    const stickies = JSON.parse(data);
+    const sticky = {
+      title: req.body.title,
+      notes: req.body.notes,
+      id: req.body.id
+    };
+
+    stickies.push(sticky);
+    fs.writeFile(DATA_FILE, JSON.stringify(stickies, null, 4), () => {
+      res.setHeader('Cache-Control', 'no-cache');
+      res.json(stickies);
+    });
+  });
+});
+
+/* UPDATE a sticky. */
+router.put('/', (req, res) => {
+  fs.readFile(DATA_FILE, (err, data) => {
+    const stickies = JSON.parse(data);
+    stickies.forEach((sticky) => {
+      if (sticky.id === req.body.id) {
+        sticky.title = req.body.title;
+        sticky.notes = req.body.notes;
+      }
+    });
+    fs.writeFile(DATA_FILE, JSON.stringify(stickies, null, 4), () => {
+      res.json({});
+    });
+  });
+});
+
+/* DELETE a sticky. */
+router.delete('/', (req, res) => {
+  fs.readFile(DATA_FILE, (err, data) => {
+    let stickies = JSON.parse(data);
+    stickies = stickies.filter(sticky => {
+      return sticky.id !== req.body.id
+    });
+
+    fs.writeFile(DATA_FILE, JSON.stringify(stickies, null, 4), () => {
+      res.json({});
+    });
+  });
 });
 
 module.exports = router;
